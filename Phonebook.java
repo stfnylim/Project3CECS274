59efbdf5478346e27a4a33f8e477e70acf1d3597
Marmolrad <44276800+Marmolrad@users.noreply.github.com>
2018-10-21 18:06:23 -0700

    Update Phonebook.java

--- Phonebook.java
+++ Phonebook.java
@@ -7,7 +7,12 @@ public class Phonebook{
 	
 	public static ArrayList<Contact> phonebook = new ArrayList<Contact>();
 	
-	
+	/**
+	* Modifies the contact
+	* @param contact - the contacts from the "Contact" class
+	* @param option - The option the user has selected
+	* @param change - the change the user wants to see
+	*/
 	public void Modify(Contact contact, String option, String change){
 		if (option == "name") {
 			
@@ -25,16 +30,26 @@ public class Phonebook{
 	
 		
 	}
-	
+	/**
+	* gets the phonebook for the cellphone
+	* @return the phonebook
+	*/
 	public static ArrayList<Contact> getContacts() {
 		
 		return phonebook;
 	}
-	
+	/**
+	* Deletes the contact from the phonebook
+	* @param contact - the contact in the phonebook
+	*/
 	public void deleteContact(Contact contact) {
 		phonebook.remove(contact);
 		// error message
 	}
+	/**
+	* Adds a contact to the phonebook
+	* @param contact - the contact to be added
+	*/
 	public static void addContact(Contact contact) {
 		phonebook.add(contact);
 		//error message
